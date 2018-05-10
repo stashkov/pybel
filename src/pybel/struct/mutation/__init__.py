6bb1730139797b4ce40617ebfb472e2f0f9c47b8
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-05-10 15:50:36 +0200

    Add collapse to star import

--- src/pybel/struct/mutation/__init__.py
+++ src/pybel/struct/mutation/__init__.py
@@ -2,8 +2,9 @@
 
 """This module contains functions that mutate or make transformations on a network"""
 
-from . import bound, deletion, expansion, induction, inference, metadata, subgraph, transfer
+from . import bound, collapse, deletion, expansion, induction, inference, metadata, subgraph, transfer
 from .bound import *
+from .collapse import *
 from .deletion import *
 from .expansion import *
 from .induction import *
@@ -13,12 +14,13 @@ from .subgraph import *
 from .transfer import *
 
 __all__ = (
-    bound.__all__ +
-    deletion.__all__ +
-    expansion.__all__ +
-    induction.__all__ +
-    inference.__all__ +
-    metadata.__all__ +
-    subgraph.__all__ +
-    transfer.__all__
+        bound.__all__ +
+        collapse.__all__ +
+        deletion.__all__ +
+        expansion.__all__ +
+        induction.__all__ +
+        inference.__all__ +
+        metadata.__all__ +
+        subgraph.__all__ +
+        transfer.__all__
 )
