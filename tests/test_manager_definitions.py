7a4ced8f100006ca5050c1ec767e5e02dfe6cbab
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-05-26 18:58:06 +0200

    Update tests

--- tests/test_manager_definitions.py
+++ tests/test_manager_definitions.py
@@ -5,8 +5,7 @@ from pathlib import Path
 
 from pybel.manager import Manager
 from tests.constants import (
-    CELL_LINE_URL, FleetingTemporaryCacheMixin, HGNC_URL, belns_dir_path, test_eq_1, test_eq_2,
-    test_ns_nocache_path,
+    CELL_LINE_URL, HGNC_URL, TemporaryCacheClsMixin, belns_dir_path, test_eq_1, test_eq_2, test_ns_nocache_path,
 )
 from tests.mocks import mock_bel_resources
 
@@ -19,7 +18,7 @@ ns2_eq = Path(test_eq_2).as_uri()
 ns2_url = 'http://resources.openbel.org/belframework/20150611/namespace/mesh-diseases.belns'
 
 
-class TestDefinitionManagers(FleetingTemporaryCacheMixin):
+class TestDefinitionManagers(TemporaryCacheClsMixin):
     def _help_check_hgnc(self, manager):
         """Helps check the HGNC namespace was loaded properly
 
@@ -45,22 +44,34 @@ class TestDefinitionManagers(FleetingTemporaryCacheMixin):
 
     @mock_bel_resources
     def test_insert_namespace_persistent(self, mock_get):
+        self.assertEqual(0, self.manager.count_namespaces())
+        self.assertEqual(0, self.manager.count_namespace_entries())
         self.manager.ensure_namespace(HGNC_URL)
         self._help_check_hgnc(self.manager)
 
-        alternate_manager = Manager(connection=self.connection)
-        alternate_manager.ensure_namespace(HGNC_URL)
-        self._help_check_hgnc(alternate_manager)
+        self.manager.namespace_model.clear()
+
+        self.manager.ensure_namespace(HGNC_URL)
+        self._help_check_hgnc(self.manager)
+
+        self.manager.drop_namespace_by_url(HGNC_URL)
+        self.assertEqual(0, self.manager.count_namespaces())
+        self.assertEqual(0, self.manager.count_namespace_entries())
 
     def test_insert_namespace_nocache(self):
         """Test that this namespace isn't cached"""
-        self.assertEqual(0, len(self.manager.list_namespaces()))
+        self.assertEqual(0, self.manager.count_namespaces())
+        self.assertEqual(0, self.manager.count_namespace_entries())
+
         self.manager.ensure_namespace(test_ns_nocache_path)
 
-        self.assertEqual(0, len(self.manager.list_namespaces()))
+        self.assertEqual(0, self.manager.count_namespaces())
+        self.assertEqual(0, self.manager.count_namespace_entries())
 
     @mock_bel_resources
     def test_insert_annotation(self, mock_get):
+        self.assertEqual(0, self.manager.count_annotations())
+        self.assertEqual(0, self.manager.count_annotation_entries())
         annotation = self.manager.ensure_annotation(CELL_LINE_URL)
         self.assertIsNotNone(annotation)
         self.assertEqual(CELL_LINE_URL, annotation.url)
@@ -68,3 +79,7 @@ class TestDefinitionManagers(FleetingTemporaryCacheMixin):
         entry = self.manager.get_annotation_entry(CELL_LINE_URL, '1321N1 cell')
         self.assertEqual('1321N1 cell', entry.name)
         self.assertEqual('CLO_0001072', entry.label)
+
+        self.manager.drop_annotation_by_url(CELL_LINE_URL)
+        self.assertEqual(0, self.manager.count_annotations())
+        self.assertEqual(0, self.manager.count_annotation_entries())
